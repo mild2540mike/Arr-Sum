df7409db005940c3292ce391043f5b81d6e644c8
Yanaranop Naulrahaeng <yanaranop2540mike@hotmail.com>
2021-08-24 14:03:02 +0700

    V1.0.0
    Dev

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,48 @@
 <script>
-	export let name;
+  export let name;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <!-- <h1>Hello {name}!</h1> -->
+  <div class="container">
+    <h1>Find Array Sum Number</h1>
+    <form autocomplete="off" class="addNumbers">
+      <label for="array">Add Array Numbers</label>
+      <input type="number" name="array" required />
+      <button>Add Numbers</button>
+    </form>
+    <form autocomplete="off" class="search">
+      <label for="find">Number to Find</label>
+      <input type="number" name="find" class="find" required />
+      <button>Search</button>
+    </form>
+    <div class="showArrRes" />
+  </div>
+
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps @MikeDev - 2021
+  </p>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,46 @@
-import App from './App.svelte';
+import App from "./App.svelte";
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
+  target: document.body,
+  props: {
+    name: "world",
+  },
 });
+// Select elements
+let formAdd = document.querySelector(".addNumbers");
+let formSearch = document.querySelector(".search");
+let showArrRes = document.querySelector(".showArrRes");
+
+let arr = [];
+formAdd.onsubmit = (e) => {
+  e.preventDefault();
+  arr.push(Number(formAdd.array.value));
+  formAdd.reset();
+  showArrRes.innerHTML = "";
+  showArrRes.prepend(`${arr}`);
+};
+
+let numToFind = 0;
+formSearch.onsubmit = (e) => {
+  e.preventDefault();
+  numToFind = +formSearch.find.value;
+  formSearch.reset();
+  showArrRes.innerHTML = "";
+  sumArrayNumbers(numToFind, arr);
+};
+
+function sumArrayNumbers(numToFind, arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let num = arr[i];
+    for (let b = 0; b < arr.length; b++) {
+      let res = arr[b] + (i !== b ? num : undefined);
+      if (res === numToFind) {
+        let div = document.createElement("div");
+        div.innerHTML = ` result : ${arr[b]} + ${num} = ${numToFind}  `;
+        showArrRes.prepend(div);
+      }
+    }
+  }
+}
 
 export default app;
